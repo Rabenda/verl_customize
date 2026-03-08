51505b85fac1ee81ce38f5a9a0f237ad5d2d9b31
rabenda <ouuuyanghh@gmail.com>
2026-03-08 19:32:08 +0000

    upload formal profiling shell

--- examples/formal_singleagent_trainer/contri1_run_grpo_gsm8k.sh
+++ examples/formal_singleagent_trainer/contri1_run_grpo_gsm8k.sh
@@ -1,7 +1,11 @@
+GPUS=4
+
 set -x
 
-export CUDA_VISIBLE_DEVICES=2,3
-
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export HF_HOME="/data/huggingface_cache"
+# model_runner 的 inference step CSV 输出到此目录
+export SGLANG_INFERENCE_LOG_DIR="/workspace/repo/verl_sglang/verl_customize/formal_exp_data_mfu"
 gsm8k_train_path=/data/verl/gsm8k/train.parquet
 gsm8k_test_path=/data/verl/gsm8k/test.parquet
 
@@ -21,16 +25,16 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files="$train_files" \
     data.val_files="$test_files" \
-    data.train_batch_size=512 \
+    data.train_batch_size=256 \
     data.max_prompt_length=512 \
     data.max_response_length=8192 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
-    actor_rollout_ref.model.path=/data/muxserve/qwen3-0.6b \
+    actor_rollout_ref.model.path=/data/muxserve/qwen3-4b-instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -39,20 +43,22 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
-    actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=$GPUS \
+    actor_rollout_ref.rollout.name=sglang \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.2 \
     actor_rollout_ref.rollout.n=4 \
     actor_rollout_ref.rollout.disable_log_stats=False \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
-    algorithm.use_kl_in_reward=False \
+    algorithm.use_kl_in_reward=True \
     trainer.critic_warmup=0 \
     trainer.logger='["console"]' \
-    trainer.project_name='verl_grpo_qwen3_gsm8k' \
-    trainer.experiment_name='qwen3_1.7b_workload_analysis' \
+    trainer.project_name='verl_grpo_model_gsm8k' \
+    trainer.experiment_name='model_gsm8k_workload_analysis' \
+    trainer.profile.print_every=1 \
     actor_rollout_ref.rollout.max_model_len=16384 \
-    trainer.n_gpus_per_node=2 \
+    trainer.decoding_length_csv_dir=/workspace/repo/verl_sglang/verl_customize/formal_exp_data_decoding_length \
+    trainer.n_gpus_per_node=$GPUS \
     trainer.nnodes=1 \
     trainer.save_freq=1000 \
     trainer.test_freq=1000 \
